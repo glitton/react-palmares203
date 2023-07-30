c4d3d7f2090dda9e8386ca612389901605e2523d
glitton <glcodeworks@gmail.com>
2023-07-30 15:22:59 -0700

    food section with data done

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Title from "./Title";
+import { cafeList, mexFoodList, intlFoodList, fishFoodList } from "../data";
 
 const Food = () => {
   return (
     <section className='section food' id='eat'>
       <Title title='nearby' secondTitle='eats' />
-
       <div className='section-center food-center'>
         <article className='food'>
           <span className='food-icon'>
@@ -14,89 +14,15 @@ const Food = () => {
           <div className='food-info'>
             <h3 className='service-title'>cafes</h3>
             <ul className='food-list'>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/groups/463890004516990/posts/702000470705941/'
-                  target='_blank'
-                  a
-                >
-                  Belleville Panaderia
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.google.com/search?q=cafe+amancia+puerto+morelos&tbm=lcl&sxsrf=AJOqlzWrrNmeXq2MqZazSHA984nfDEsAiA%3A1679425494389&ei=1v8ZZIWqF-7HkPIPy6S_qA8&oq=cafe+amancia&gs_lcp=Cg1nd3Mtd2l6LWxvY2FsEAMYADIKCAAQgAQQFBCHAjIFCAAQgAQyBggAEBYQHjICCCY6BAgjECc6BAgAEEM6BQgAEJECUABY-w1gnxhoAHAAeAKAAfsDiAHMGJIBCzAuMi41LjIuMS4xmAEAoAEBwAEB&sclient=gws-wiz-local'
-                  target='_blank'
-                  a
-                >
-                  Cafe Amancia
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://laylaguesthouse.com/es/cafe-layla-menu/'
-                  target='_blank'
-                  a
-                >
-                  Cafe Layla
-                </a>
-              </li>
-              <li className='food-link'></li>
-              <li className='food-link'>
-                <a
-                  href='https://www.tripadvisor.com/Restaurant_Review-g240327-d23727568-Reviews-Cecilia_Bakes_Coffee_Shop-Puerto_Morelos_Yucatan_Peninsula.html'
-                  target='_blank'
-                  a
-                >
-                  Cecilia Bakes
-                </a>
-              </li>
-              <li className='food-link'>
-                <a href='https://pm.eliniciorestaurante.com/' target='_blank' a>
-                  El Inicio
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.google.com/search?q=oneida%20cafe%20puerto%20morelos&oq=oneida+cafe+puerto+&aqs=chrome.1.69i57j33i160l2.7684j0j4&sourceid=chrome&ie=UTF-8&tbs=lf:1,lf_ui:9&tbm=lcl&sxsrf=AJOqlzVj4mMUDrbRNVXH9IExd6_X0ilrUg:1679427415137&rflfq=1&num=10&rldimm=17498560580722829126&lqi=ChpvbmVpZGEgY2FmZSBwdWVydG8gbW9yZWxvc0i45JPZg6-AgAhaJhAAEAEYASIab25laWRhIGNhZmUgcHVlcnRvIG1vcmVsb3MyAmVzkgEEY2FmZZoBI0NoWkRTVWhOTUc5blMwVkpRMEZuU1VOQ09IVmZjMEYzRUFFqgE_CgkvbS8wMjBmYjIQASoPIgtvbmVpZGEgY2FmZSgOMh8QASIbgoV4PA3WGqQdX-bucgOUhzTOif14eeiDLw-I&phdesc=4hPtMr2WH8M&ved=2ahUKEwj1j4XU4u39AhUxLkQIHaqiBMQQvS56BAgYEAE&sa=X&rlst=f#rlfi=hd:;si:17498560580722829126,l,ChpvbmVpZGEgY2FmZSBwdWVydG8gbW9yZWxvc0i45JPZg6-AgAhaJhAAEAEYASIab25laWRhIGNhZmUgcHVlcnRvIG1vcmVsb3MyAmVzkgEEY2FmZZoBI0NoWkRTVWhOTUc5blMwVkpRMEZuU1VOQ09IVmZjMEYzRUFFqgE_CgkvbS8wMjBmYjIQASoPIgtvbmVpZGEgY2FmZSgOMh8QASIbgoV4PA3WGqQdX-bucgOUhzTOif14eeiDLw-I,y,4hPtMr2WH8M;mv:[[20.8748708,-86.87257199999999],[20.831762599999998,-86.9025786]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!2m4!1e17!4m2!17m1!1e2!3sIAE,lf:1,lf_ui:9'
-                  target='_blank'
-                  a
-                >
-                  Eneida Cafe
-                </a>
-              </li>
-              <li className='food-link'>
-                <a href='https://www.estacion69.com/' target='_blank' a>
-                  Estacion 69
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://localcofffeshop.wordpress.com/'
-                  target='_blank'
-                  a
-                >
-                  Local Coffee Shop
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/lolaymoya/?locale=es_LA'
-                  target='_blank'
-                  a
-                >
-                  Lola Y Moya
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/mangatapuertomorelos'
-                  target='_blank'
-                  a
-                >
-                  Mangata
-                </a>
-              </li>
+              {cafeList.map((cafe) => {
+                return (
+                  <li className='food-link' key={cafe.id}>
+                    <a href={cafe.href} target='_blank' rel='noreferrer' a>
+                      {cafe.restaurant}
+                    </a>
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </article>
@@ -108,96 +34,18 @@ const Food = () => {
           <div className='food-info'>
             <h3 className='service-title'>tacos</h3>
             <ul className='food-list'>
-              <li className='food-link'>
-                <a href='https://www.facebook.com/ElNichoPM/' target='_blank' a>
-                  El Nicho
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/profile.php?id=100063548470071'
-                  target='_blank'
-                  a
-                >
-                  La Cabaña
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/lachozadelpuerto/'
-                  target='_blank'
-                  a
-                >
-                  La Choza
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.tripadvisor.com/Restaurant_Review-g240327-d12396192-Reviews-Las_Koras-Puerto_Morelos_Yucatan_Peninsula.html'
-                  target='_blank'
-                  a
-                >
-                  Las Koras
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/pecaofondademar/'
-                  target='_blank'
-                  a
-                >
-                  Pecao
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/SocioNaizPuertoMorelos/'
-                  target='_blank'
-                  a
-                >
-                  Socio Naiz
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/profile.php?id=100063965113592'
-                  target='_blank'
-                  a
-                >
-                  taco.com
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/people/Taquer%C3%ADa-El-Mariachi/100063789630051/?paipv=0&eav=Afaeu87_REEGhYUlNEP5RqHoJ103i2t7_j4vS2qpJHM_Mj88n0uPce6pGdPnfih0FFA&_rdr'
-                  target='_blank'
-                  a
-                >
-                  Taqueria El Mariachi
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.tripadvisor.com/Restaurant_Review-g240327-d16738846-Reviews-Taqueria_Neza-Puerto_Morelos_Yucatan_Peninsula.html'
-                  target='_blank'
-                  a
-                >
-                  Taqueria Neza
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/VeggieVeggieStreetFood'
-                  target='_blank'
-                  a
-                >
-                  Veggie Veggie Street Food
-                </a>
-              </li>
+              {mexFoodList.map((food) => {
+                return (
+                  <li className='food-link' key={food.id}>
+                    <a href={food.href} target='_blank' rel='noreferrer' a>
+                      {food.restaurant}
+                    </a>
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </article>
-
         <article className='food'>
           <span className='food-icon'>
             <i className='fa-solid fa-bowl-food'></i>
@@ -205,89 +53,18 @@ const Food = () => {
           <div className='food-info'>
             <h3 className='service-title'>international</h3>
             <ul className='food-list food-international'>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/Daipuertomorelos/?locale=es_LA'
-                  target='_blank'
-                  a
-                >
-                  Dai Smokehouse and Sake
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/doragonrestaurantejapones'
-                  target='_blank'
-                  a
-                >
-                  Doragon Sushi
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/groups/463890004516990/posts/702000470705941/'
-                  target='_blank'
-                  a
-                >
-                  El Campanario
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.tripadvisor.com.mx/Restaurant_Review-g240327-d1051278-Reviews-El_Pirata-Puerto_Morelos_Yucatan_Peninsula.html'
-                  target='_blank'
-                  a
-                >
-                  El Pirata
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/ruta40puertomorelos/'
-                  target='_blank'
-                  a
-                >
-                  Empanadas Ruta 40
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/iwannapizza/'
-                  target='_blank'
-                  a
-                >
-                  I Wanna Pizza
-                </a>
-              </li>
-
-              <li className='food-link'>
-                <a href='https://www.memalmare.com/' target='_blank' a>
-                  Mem Al Mare
-                </a>
-              </li>
-              <li className='food-link'>
-                <a href='https://www.osteriabarocca.com/' target='_blank' a>
-                  Osteria Barocca
-                </a>
-              </li>
-              <li className='food-link'>
-                <a href='https://taninosrestaurant.com/' target='_blank' a>
-                  Tanino's
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://restaurantesenpuertomorelos.com/establecimiento/yum-yum-wok/'
-                  target='_blank'
-                  a
-                >
-                  Yum Yum Wok
-                </a>
-              </li>
+              {intlFoodList.map((food) => {
+                return (
+                  <li className='food-link' key={food.id}>
+                    <a href={food.href} target='_blank' rel='noreferrer' a>
+                      {food.restaurant}
+                    </a>
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </article>
-
         <article className='food'>
           <span className='food-icon'>
             <i className='fa-solid fa-fish-fins'></i>
@@ -295,57 +72,15 @@ const Food = () => {
           <div className='food-info'>
             <h3 className='service-title'>seafood</h3>
             <ul className='food-list food-international'>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/BLUE.MORELOS/?locale=es_LA'
-                  target='_blank'
-                  a
-                >
-                  Blue
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/BoquinetePuerto/'
-                  target='_blank'
-                  a
-                >
-                  Boquinete Seafood & Grill
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://www.facebook.com/elpesqueropuertomorelos/reviews'
-                  target='_blank'
-                  a
-                >
-                  El Pesquero
-                </a>
-              </li>
-              <li className='food-link'>
-                <a href='https://lasirenapm.com/' target='_blank' a>
-                  La Sirena
-                </a>
-              </li>
-              <li className='food-link'>
-                <a href='http://muelleonce.com/' target='_blank' a>
-                  Muelle Once
-                </a>
-              </li>
-              <li className='food-link'>
-                <a href='https://playajaguar.com/inicio/' target='_blank' a>
-                  Playa Jaguar
-                </a>
-              </li>
-              <li className='food-link'>
-                <a
-                  href='https://bullandtank.com/punta-corcho'
-                  target='_blank'
-                  a
-                >
-                  Punta Corcho
-                </a>
-              </li>
+              {fishFoodList.map((fish) => {
+                return (
+                  <li className='food-link' key={fish.id}>
+                    <a href={fish.href} target='_blank' rel='noreferrer' a>
+                      {fish.restaurant}
+                    </a>
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </article>
--- src/data.js
+++ src/data.js
@@ -25,3 +25,205 @@ export const pageLinks = [
     text: "book",
   },
 ];
+
+export const cafeList = [
+  {
+    id: 11,
+    href: "https://www.facebook.com/groups/463890004516990/posts/702000470705941/",
+    restaurant: "Cafe Amancia",
+  },
+  {
+    id: 12,
+    href: "https://laylaguesthouse.com/es/cafe-layla-menu/",
+    restaurant: "Cafe Layla",
+  },
+  {
+    id: 13,
+    href: "https://www.tripadvisor.com/Restaurant_Review-g240327-d23727568-Reviews-Cecilia_Bakes_Coffee_Shop-Puerto_Morelos_Yucatan_Peninsula.html",
+    restaurant: "Cecilia Bakes",
+  },
+  {
+    id: 14,
+    href: "https://pm.eliniciorestaurante.com/",
+    restaurant: "El Inicio",
+  },
+  {
+    id: 15,
+    href: "https://www.facebook.com/groups/463890004516990/posts/702000470705941/",
+    restaurant: "Cafe Amancia",
+  },
+  {
+    id: 16,
+    href: "https://www.google.com/search?q=oneida%20cafe%20puerto%20morelos&oq=oneida+cafe+puerto+&aqs=chrome.1.69i57j33i160l2.7684j0j4&sourceid=chrome&ie=UTF-8&tbs=lf:1,lf_ui:9&tbm=lcl&sxsrf=AJOqlzVj4mMUDrbRNVXH9IExd6_X0ilrUg:1679427415137&rflfq=1&num=10&rldimm=17498560580722829126&lqi=ChpvbmVpZGEgY2FmZSBwdWVydG8gbW9yZWxvc0i45JPZg6-AgAhaJhAAEAEYASIab25laWRhIGNhZmUgcHVlcnRvIG1vcmVsb3MyAmVzkgEEY2FmZZoBI0NoWkRTVWhOTUc5blMwVkpRMEZuU1VOQ09IVmZjMEYzRUFFqgE_CgkvbS8wMjBmYjIQASoPIgtvbmVpZGEgY2FmZSgOMh8QASIbgoV4PA3WGqQdX-bucgOUhzTOif14eeiDLw-I&phdesc=4hPtMr2WH8M&ved=2ahUKEwj1j4XU4u39AhUxLkQIHaqiBMQQvS56BAgYEAE&sa=X&rlst=f#rlfi=hd:;si:17498560580722829126,l,ChpvbmVpZGEgY2FmZSBwdWVydG8gbW9yZWxvc0i45JPZg6-AgAhaJhAAEAEYASIab25laWRhIGNhZmUgcHVlcnRvIG1vcmVsb3MyAmVzkgEEY2FmZZoBI0NoWkRTVWhOTUc5blMwVkpRMEZuU1VOQ09IVmZjMEYzRUFFqgE_CgkvbS8wMjBmYjIQASoPIgtvbmVpZGEgY2FmZSgOMh8QASIbgoV4PA3WGqQdX-bucgOUhzTOif14eeiDLw-I,y,4hPtMr2WH8M;mv:[[20.8748708,-86.87257199999999],[20.831762599999998,-86.9025786]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!2m4!1e17!4m2!17m1!1e2!3sIAE,lf:1,lf_ui:9",
+    restaurant: "Eneida Cafe",
+  },
+  {
+    id: 17,
+    href: "https://www.estacion69.com/",
+    restaurant: "Estacion 69",
+  },
+  {
+    id: 18,
+    href: "https://localcofffeshop.wordpress.com/",
+    restaurant: "Local Coffee Shop",
+  },
+  {
+    id: 19,
+    href: "https://www.facebook.com/lolaymoya/?locale=es_LA",
+    restaurant: "Lola Y Moya",
+  },
+  {
+    id: 20,
+    href: "https://www.facebook.com/mangatapuertomorelos",
+    restaurant: "Mangata",
+  },
+  {
+    id: 21,
+    href: "https://www.estacion69.com/",
+    restaurant: "Estacion 69",
+  },
+];
+
+export const mexFoodList = [
+  {
+    id: 111,
+    href: "https://www.facebook.com/ElNichoPM/",
+    restaurant: "El Nicho",
+  },
+  {
+    id: 112,
+    href: "https://www.facebook.com/profile.php?id=100063548470071",
+    restaurant: "La Cabaña",
+  },
+  {
+    id: 113,
+    href: "https://www.facebook.com/ElNichoPM/",
+    restaurant: "El Nicho",
+  },
+  {
+    id: 114,
+    href: "https://www.facebook.com/lachozadelpuerto/",
+    restaurant: "La Choza",
+  },
+  {
+    id: 115,
+    href: "https://www.tripadvisor.com/Restaurant_Review-g240327-d12396192-Reviews-Las_Koras-Puerto_Morelos_Yucatan_Peninsula.html",
+    restaurant: "Las Koras",
+  },
+  {
+    id: 116,
+    href: "https://www.facebook.com/pecaofondademar/",
+    restaurant: "Pecao",
+  },
+  {
+    id: 117,
+    href: "https://www.facebook.com/SocioNaizPuertoMorelos/",
+    restaurant: "Socio Naiz",
+  },
+  {
+    id: 118,
+    href: "https://www.facebook.com/profile.php?id=100063965113592",
+    restaurant: "taco.com",
+  },
+  {
+    id: 119,
+    href: "https://www.facebook.com/people/Taquer%C3%ADa-El-Mariachi/100063789630051/?paipv=0&eav=Afaeu87_REEGhYUlNEP5RqHoJ103i2t7_j4vS2qpJHM_Mj88n0uPce6pGdPnfih0FFA&_rdr",
+    restaurant: "Taqueria El Mariachi",
+  },
+  {
+    id: 120,
+    href: "https://www.tripadvisor.com/Restaurant_Review-g240327-d16738846-Reviews-Taqueria_Neza-Puerto_Morelos_Yucatan_Peninsula.html",
+    restaurant: "Taqueria Neza",
+  },
+  {
+    id: 121,
+    href: "https://www.facebook.com/VeggieVeggieStreetFood",
+    restaurant: "Veggie Veggie Street Food",
+  },
+];
+
+export const intlFoodList = [
+  {
+    id: 222,
+    href: "https://www.facebook.com/Daipuertomorelos/?locale=es_LA",
+    restaurant: "Dai Smokehouse and Sake",
+  },
+  {
+    id: 223,
+    href: "https://www.facebook.com/doragonrestaurantejapones",
+    restaurant: "Doragon Sushi",
+  },
+  {
+    id: 224,
+    href: "https://www.facebook.com/groups/463890004516990/posts/702000470705941/",
+    restaurant: "El Campanario",
+  },
+  {
+    id: 225,
+    href: "https://www.tripadvisor.com.mx/Restaurant_Review-g240327-d1051278-Reviews-El_Pirata-Puerto_Morelos_Yucatan_Peninsula.html",
+    restaurant: "El Pirata",
+  },
+  {
+    id: 226,
+    href: "https://www.facebook.com/ruta40puertomorelos/",
+    restaurant: "Empanadas Ruta 40",
+  },
+  {
+    id: 227,
+    href: "https://www.facebook.com/iwannapizza/",
+    restaurant: "I Wanna Pizza",
+  },
+  {
+    id: 228,
+    href: "https://www.memalmare.com/",
+    restaurant: "Mem Al Mare",
+  },
+  {
+    id: 229,
+    href: "https://taninosrestaurant.com/",
+    restaurant: "Tanino's",
+  },
+  {
+    id: 230,
+    href: "https://restaurantesenpuertomorelos.com/establecimiento/yum-yum-wok/",
+    restaurant: "Yum Yum Wok",
+  },
+];
+
+export const fishFoodList = [
+  {
+    id: 333,
+    href: "https://www.facebook.com/BLUE.MORELOS/?locale=es_LA",
+    restaurant: "Blue",
+  },
+  {
+    id: 334,
+    href: "https://www.facebook.com/BoquinetePuerto/",
+    restaurant: "Boquinete Seafood & Grill",
+  },
+  {
+    id: 335,
+    href: "https://www.facebook.com/elpesqueropuertomorelos/reviews",
+    restaurant: "El Pesquero",
+  },
+  {
+    id: 336,
+    href: "https://lasirenapm.com/",
+    restaurant: "La Sirena",
+  },
+  {
+    id: 337,
+    href: "http://muelleonce.com/",
+    restaurant: "Muelle Once",
+  },
+  {
+    id: 338,
+    href: "https://playajaguar.com/inicio/",
+    restaurant: "Playa Jaguar",
+  },
+  {
+    id: 339,
+    href: "https://bullandtank.com/punta-corcho",
+    restaurant: "Punta Corcho",
+  },
+];
