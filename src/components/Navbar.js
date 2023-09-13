5165da44ed968b98b6c33e5588688b4ad6e617cf
glitton <glcodeworks@gmail.com>
2023-09-13 12:07:42 -0500

    add X fontawesome icon to mobile nav when links are clicked

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { useState, useRef } from "react";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 import { pageLinks } from "../data";
 import logo from "../images/palmares-203-resized.svg";
 
@@ -7,16 +7,12 @@ const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
   const navLinksContainerRef = useRef(null);
   const navLinksRef = useRef(null);
+  const navLinkStyles = {
+    height: showLinks ? "320px" : "0px",
+  };
 
   const toggleNavLinks = () => {
     setShowLinks(!showLinks);
-    console.log(navLinksRef.current);
-  };
-
-  const navLinkStyles = {
-    height: showLinks
-      ? `${navLinksRef.current.getBoundingClientRect().height}px`
-      : "0px",
   };
 
   return (
@@ -27,29 +23,27 @@ const Navbar = () => {
             <img src={logo} className='nav-logo' alt='palmares 203 logo' />
           </a>
           <button type='button' className='nav-toggle' onClick={toggleNavLinks}>
-            <FaBars />
+            {showLinks ? <FaTimes /> : <FaBars />}
           </button>
         </div>
-        {showLinks && (
-          <div
-            className='nav-links-container'
-            ref={navLinksContainerRef}
-            style={navLinkStyles}
-          >
-            <ul className='nav-links' ref={navLinksRef}>
-              {pageLinks.map((link) => {
-                const { id, href, text } = link;
-                return (
-                  <li key={id}>
-                    <a href={href} className='nav-link scroll-link'>
-                      {text}
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
+        <div
+          className='nav-links-container'
+          ref={navLinksContainerRef}
+          style={navLinkStyles}
+        >
+          <ul className='nav-links' ref={navLinksRef}>
+            {pageLinks.map((link) => {
+              const { id, href, text } = link;
+              return (
+                <li key={id}>
+                  <a href={href} className='nav-link'>
+                    {text}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
         <div className='nav-icons'>
           <a
             href='https://www.airbnb.com/rooms/589684916167853690?guests=1&adults=1&s=67&unique_share_id=169eed4a-7224-4ac5-a17c-a0170fe67904'
